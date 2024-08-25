f6d2fc96213058886591fb912fc89316e0b7ddc0
samuelkanyingi <samuelkanyingi2016@gmail.com>
2024-08-25 13:19:07 +0300

    docs(app.py): added docstrings to explain and improve readability
    - Added dosrings to explain purpose of class Deck to understand it better
    - Added docstring to explain why to_dict() which converts object to dictionary
    - This to_dict() helps us manipulate data before serializing to json

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ current_question_index = 0
 
 
 class User(UserMixin, db.Model):
-    '''represents a user in the database 
+    '''represents a table user in the database 
     inherit from  Usermixin  class to simplify user authentication and gives access to methods and properties
     '''
     id = db.Column(db.Integer, primary_key=True) #unique identifier for user
@@ -45,7 +45,7 @@ class User(UserMixin, db.Model):
     decks = db.relationship('Deck', backref='owner', lazy=True) # list of deck owned by user/ one to many relationship
 
 class PasswordResetToken(db.Model):
-    """Represents a password reset token in database.
+    """Represents a table of password reset token in database.
     """
     id = db.Column(db.Integer, primary_key=True)
     user_id  =db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
@@ -53,7 +53,7 @@ class PasswordResetToken(db.Model):
     expires_at = db.Column(db.DateTime, nullable=False)
 
 class Deck(db.Model):
-    '''' represents deck of quizzes in the database '''
+    '''' represents a table of deck of quizzes in the database '''
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=True)
     quiz_count = db.Column(db.Integer, default=0)
@@ -62,15 +62,17 @@ class Deck(db.Model):
     quizzes = db.relationship('Quiz',backref='deck', lazy=True) # list of quizzes associated with a deck
 
     def to_dict(self):
+        ''' Returns dictionary representation of object containing key attributes'''
         return {'id': self.id, 'name': self.name, 'quiz_count': self.quiz_count, 'title': self.title}
 
 
 class Quiz(db.Model):
+    """ Represent a quiz question in database """
     id = db.Column(db.Integer, primary_key=True)
     question = db.Column(db.String(200), nullable=False)
     answer = db.Column(db.String(255), nullable=False)
-    deck_id = db.Column(db.Integer, db.ForeignKey('deck.id'), nullable=True)
-    next_review_time = db.Column(db.DateTime, nullable=True, default=datetime.utcnow)
+    deck_id = db.Column(db.Integer, db.ForeignKey('deck.id'), nullable=True) # id of deck it belongs to
+    next_review_time = db.Column(db.DateTime, nullable=True, default=datetime.utcnow) #next time quiz is to be reviewd
 
 @login_manager.user_loader
 def load_user(id):
